e4023441c29d5c73a1f9f05a2d274bb70b6219b8
João Vitor Guedes <144271585+JVguedesF@users.noreply.github.com>
2025-04-17 15:23:34 -0300

    ADD

--- src/test/java/com/example/FireWatch03/FireWatch03ApplicationTests.java
+++ src/test/java/com/example/FireWatch03/FireWatch03ApplicationTests.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = {
+				"spring.flyway.enabled=false",
+				"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+				"spring.datasource.driver-class-name=org.h2.Driver"
+		}
+)
 @ActiveProfiles("test")
 class FireWatch03ApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() { }
 }
